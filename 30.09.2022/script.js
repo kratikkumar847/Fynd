52c9b9b431759886fa3809756393846620919246
kratikkumar847 <kratikkumar847@gmail.com>
2022-10-01 17:41:35 +0530

    Alarm

--- 30.09.2022/script.js
+++ 30.09.2022/script.js
@@ -1,12 +1,44 @@
-let hour=0;
-let minute=0;
-let second=0;
-
+let hour;
+let minute;
+let second;
+let hourSet;
+let minuteSet;
+let secondSet;
+let dateDiffernce;let temp ;
+var audio = new Audio('alarm.mp3');
 setInterval(()=>{
-     hour = new Date().getHours();
-     minute = new Date().getMinutes();
-     second = new Date().getSeconds();
+     temp = new Date();
+     hour = temp.getHours();
+     minute = temp.getMinutes();
+     second = temp.getSeconds();
+     let fullTime = `${hour}:${minute}:${second} `;
     document.getElementById('display').innerHTML = fullTime;
-}, 0)
+}, 500);
 
-let fullTime = `${hour}:${minute}:${second} `;
+let nowDate;
+function ring(){
+    audio.play();
+}
+
+function myFunction(){
+     hourSet = document.getElementById('hour').value;
+     minuteSet = document.getElementById('minute').value;
+    secondSet = document.getElementById('second').value;
+    
+    let setDate = new Date();
+    setDate.setHours(hourSet);
+    setDate.setMinutes(minuteSet);
+    setDate.setSeconds(secondSet);
+    console.log(setDate);
+    nowDate = new Date();
+    
+    dateDiffernce = setDate- nowDate;
+    console.log(dateDiffernce);
+    if(dateDiffernce >= 0){
+        setTimeout(()=>{
+           ring();
+        }, dateDiffernce);    
+    }
+}
+
+
